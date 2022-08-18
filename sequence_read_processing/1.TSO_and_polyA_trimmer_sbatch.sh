4749fbce98f1e06cd1115688cbf2c6b53d22acb2
hsounart <sounart@kth.se>
2022-08-18 12:29:58 +0200

    Added adaptor trimming script

---
+++ sequence_read_processing/1.TSO_and_polyA_trimmer_sbatch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#run TSO and polyA trimming on all Read2 fastq sequence files
+
+#TSO_and_polyA_trimmer_correct.sh script can be found on github page: https://github.com/ludvigla/VisiumTrim
+
+#ran with cutadapt version 2.3
+module load cutadapt/2.3
+
+for R2_fastq_file in /V10F24_041/fastq/*_R2_*.fastq.gz
+do bash TSO_polyA_trimming.sh "$R2_fastq_file"
+done
